48799bff8d53dd9bf3422f1b3ea5f6443420da03
colorfulee <54055007@qq.com>
2024-04-19 16:46:20 +0800

    增加产品页面

--- pages/product/product.vue
+++ pages/product/product.vue
@@ -1,5 +1,11 @@
 <template>
-	<view>product</view>
+	<z-nav-bar backState=2000 home='false'></z-nav-bar>
+	<view class='container'>
+		<image mode="widthFix" class="image1" src="../../static/product/SY_12_bgJY.jpg"></image>
+		<image mode="widthFix" class="image2" src="../../static/product/SY_12_bgQZY.jpg"></image>
+		<image mode="widthFix" class="image2" src="../../static/product/SY_12_bgJKJC.jpg"></image>
+		<image mode="widthFix" class="image3" src="../../static/product/SY_12_bgFJZS.jpg"></image>
+	</view>
 </template>
 
 <script>
@@ -26,5 +32,31 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.container {
+		display: flex;
+		flex-wrap: wrap;
+		margin: 0 auto;
+		margin-bottom: 200rpx;
+		justify-content: center;
+
+		image {
+			padding: 10rpx;
+		}
+
+		.image1 {
+			width: 668rpx;
+			height: 360rpx;
+		}
+
+		.image2 {
+			width: 326rpx;
+			height: 402rpx;
+		}
+
+		.image3 {
+			width: 668rpx;
+			height: 166rpx;
+		}
+	}
 </style>
